0f84f50c61eea51c02c1cd05cdd78111c8b4aa17
sethupathy2004-svg <sethupathy2004@gmail.com>
2026-01-06 23:13:22 +0530

    Update BatteryDataRepository.java

--- backend/repository/BatteryDataRepository.java
+++ backend/repository/BatteryDataRepository.java
@@ -2,9 +2,11 @@ package com.bms.repository;
 
 import com.bms.model.BatteryData;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
-public interface BatteryDataRepository extends JpaRepository<BatteryData, Integer> {
+public interface BatteryDataRepository
+        extends JpaRepository<BatteryData, Integer> {
 
-    List<BatteryData> findByBattery_BatteryId(int batteryId);
+    List<BatteryData> findByBattery_Id(int batteryId);
 }
