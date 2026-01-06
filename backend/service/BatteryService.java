25cf34bb58f94506c6c5efa7d0d3fe85bf26e530
sethupathy2004-svg <sethupathy2004@gmail.com>
2026-01-06 16:06:33 +0530

    Update BatteryService.java

--- backend/service/BatteryService.java
+++ backend/service/BatteryService.java
@@ -1,11 +1,40 @@
 package com.bms.service;
 
+import com.bms.model.Battery;
+import com.bms.model.BatteryData;
+import com.bms.repository.BatteryDataRepository;
+import com.bms.repository.BatteryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class BatteryService {
 
+    @Autowired
+    private BatteryRepository batteryRepository;
+
+    @Autowired
+    private BatteryDataRepository batteryDataRepository;
+
     public double calculateSOC(double voltage, double ratedVoltage) {
         return (voltage / ratedVoltage) * 100;
+    }
+
+    public Battery addBattery(Battery battery) {
+        return batteryRepository.save(battery);
+    }
+
+    public BatteryData saveBatteryData(int batteryId, BatteryData data) {
+        Battery battery = batteryRepository.findById(batteryId).orElseThrow();
+        data.setBattery(battery);
+        data.setTimestamp(LocalDateTime.now());
+        return batteryDataRepository.save(data);
+    }
+
+    public List<BatteryData> getBatteryData(int batteryId) {
+        return batteryDataRepository.findByBattery_BatteryId(batteryId);
     }
 }
