f2e6fb31b652b59a1df554fba68960dc9fbeea71
sethupathy2004-svg <sethupathy2004@gmail.com>
2026-01-06 23:18:06 +0530

    Update battery.sql

--- database/battery.sql
+++ database/battery.sql
@@ -1,22 +1,11 @@
-CREATE DATABASE battery_management;
-USE battery_management;
+CREATE DATABASE battery_db;
+
+USE battery_db;
 
 CREATE TABLE battery (
-    battery_id INT AUTO_INCREMENT PRIMARY KEY,
-    battery_name VARCHAR(50),
-    battery_type VARCHAR(30),
-    rated_voltage DOUBLE,
-    rated_capacity DOUBLE
-);
-
-CREATE TABLE battery_data (
-    data_id INT AUTO_INCREMENT PRIMARY KEY,
-    battery_id INT,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
     voltage DOUBLE,
-    current DOUBLE,
-    temperature DOUBLE,
-    soc DOUBLE,
-    timestamp DATETIME,
-    FOREIGN KEY (battery_id) REFERENCES battery(battery_id)
+    rated_voltage DOUBLE,
+    soc DOUBLE
 );
-
