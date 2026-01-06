71106678728fcd9f5bdf37c94608fe31e12b3193
sethupathy2004-svg <sethupathy2004@gmail.com>
2026-01-06 16:07:14 +0530

    Update BatteryController.java

--- backend/controller/BatteryController.java
+++ backend/controller/BatteryController.java
@@ -1,8 +1,12 @@
 package com.bms.controller;
 
+import com.bms.model.Battery;
+import com.bms.model.BatteryData;
 import com.bms.service.BatteryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/battery")
@@ -15,7 +19,22 @@ public class BatteryController {
     @GetMapping("/soc")
     public double getSOC(@RequestParam double voltage,
                          @RequestParam double ratedVoltage) {
-
         return batteryService.calculateSOC(voltage, ratedVoltage);
+    }
+
+    @PostMapping("/add")
+    public Battery addBattery(@RequestBody Battery battery) {
+        return batteryService.addBattery(battery);
+    }
+
+    @PostMapping("/{batteryId}/data")
+    public BatteryData addBatteryData(@PathVariable int batteryId,
+                                      @RequestBody BatteryData data) {
+        return batteryService.saveBatteryData(batteryId, data);
+    }
+
+    @GetMapping("/{batteryId}/data")
+    public List<BatteryData> getBatteryData(@PathVariable int batteryId) {
+        return batteryService.getBatteryData(batteryId);
     }
 }
