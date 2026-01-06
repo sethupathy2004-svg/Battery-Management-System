58c71547d1f07c3df5611fa88b5675958163df03
sethupathy2004-svg <sethupathy2004@gmail.com>
2026-01-06 16:05:54 +0530

    Create BatteryDataRepository.java

---
+++ backend/repository/BatteryDataRepository.java
@@ -0,0 +1,10 @@
+package com.bms.repository;
+
+import com.bms.model.BatteryData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface BatteryDataRepository extends JpaRepository<BatteryData, Integer> {
+
+    List<BatteryData> findByBattery_BatteryId(int batteryId);
+}
