490355fedcaa8c8fe5243d08a39599d25e6994ab
sethupathy2004-svg <sethupathy2004@gmail.com>
2026-01-06 23:12:47 +0530

    Update BatteryData.java

--- backend/model/BatteryData.java
+++ backend/model/BatteryData.java
@@ -4,39 +4,26 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "battery_data")
 public class BatteryData {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int dataId;
-
-    @ManyToOne
-    @JoinColumn(name = "battery_id")
-    private Battery battery;
+    private int id;
 
     private double voltage;
-    private double current;
-    private double temperature;
-    private double soc;
 
     private LocalDateTime timestamp;
 
-    // Getters and Setters
-    public int getDataId() {
-        return dataId;
+    @ManyToOne
+    private Battery battery;
+
+    // Getters & Setters
+    public int getId() {
+        return id;
     }
 
-    public void setDataId(int dataId) {
-        this.dataId = dataId;
-    }
-
-    public Battery getBattery() {
-        return battery;
-    }
-
-    public void setBattery(Battery battery) {
-        this.battery = battery;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public double getVoltage() {
@@ -47,35 +34,19 @@ public class BatteryData {
         this.voltage = voltage;
     }
 
-    public double getCurrent() {
-        return current;
-    }
-
-    public void setCurrent(double current) {
-        this.current = current;
-    }
-
-    public double getTemperature() {
-        return temperature;
-    }
-
-    public void setTemperature(double temperature) {
-        this.temperature = temperature;
-    }
-
-    public double getSoc() {
-        return soc;
-    }
-
-    public void setSoc(double soc) {
-        this.soc = soc;
-    }
-
     public LocalDateTime getTimestamp() {
         return timestamp;
     }
 
     public void setTimestamp(LocalDateTime timestamp) {
         this.timestamp = timestamp;
+    }
+
+    public Battery getBattery() {
+        return battery;
+    }
+
+    public void setBattery(Battery battery) {
+        this.battery = battery;
     }
 }
